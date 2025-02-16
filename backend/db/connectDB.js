c05442dd64952a9e8aff3e5a5a5edcbfe15fb8ca
unknown <muhammadjunaid9394@gmail.com>
2025-02-16 22:14:27 +0500

    Setting timeout section

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/ecommbackend`);
+        await mongoose.connect(`${process.env.MONGODB_URL}/ecommbackend`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 20000, 
+        });
     } catch (error) {
         console.log("Error while connecting to the database", error);
     }
